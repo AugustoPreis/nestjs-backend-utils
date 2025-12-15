efd1c949088a07aa65d12dda7870d6972112608c
Augusto Tomasi <augustopreisthomasi@gmail.com>
2025-12-15 16:51:41 -0300

    feat: export all components

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+/**
+ * @augustopreis/nestjs-utils
+ *
+ * Biblioteca completa de utilitários para NestJS.
+ * Fornece componentes reutilizáveis para acelerar o desenvolvimento de
+ * aplicações NestJS seguindo as melhores práticas.
+ *
+ * @packageDocumentation
+ */
+
+export * from './exceptions';
+export * from './transformers';
+export * from './helpers';
+export * from './decorators';
+export * from './guards';
+export * from './interceptors';
+export * from './database';
+export * from './dtos';
